a5a3dbfd2d777dc1058f57dc76d20d639918ae7e
iceBear67 <icebear67@sfclub.cc>
2022-12-20 19:14:35 +0800

    Fix some bugs

--- src/main/java/io/ib67/meeting/data/MeetingSession.java
+++ src/main/java/io/ib67/meeting/data/MeetingSession.java
@@ -8,37 +8,38 @@ import net.kyori.adventure.text.format.TextDecoration;
 import net.minestom.server.entity.Player;
 import net.minestom.server.instance.Instance;
 
+import java.util.stream.Collectors;
+
 public record MeetingSession(
         String title,
         String inviteCode,
         boolean persistent,
         Instance instance
-        ) {
-        public void greet(Player player) {
-                player.sendMessage(
-                        Component.text("Welcome! ") //huh, over-engineered api, isn't it?
-                                .color(NamedTextColor.GREEN)
-                                .style(Style.style(TextDecoration.BOLD))
-                                .append(
-                                        Component.text("You just joined ")
-                                                .append(
-                                                        Component.text(title())
-                                                                .style(Style.style(TextDecoration.UNDERLINED))
-                                                )
-                                )
-                );
-                instance.getPlayers().forEach(p -> {
-                        p.sendMessage(
-                                Component.text(player.getUsername()).color(NamedTextColor.AQUA)
-                                        .append(Component.text(" joined the meeting!").color(NamedTextColor.WHITE))
-                        );
-                });
-                player.sendMessage(Component.text("Here are " + instance.getPlayers().size() + " players in the meeting, including ")
+) {
+    public void greet(Player player) {
+        player.sendMessage(
+                Component.text("Welcome! ") //huh, over-engineered api, isn't it?
+                        .color(NamedTextColor.GREEN)
+                        .style(Style.style(TextDecoration.BOLD))
                         .append(
-                                instance.getPlayers().stream()
-                                        .map(it -> Component.text(it.getUsername()).color(NamedTextColor.AQUA))
-                                        .reduce(ScopedComponent::append)
-                                        .get()
-                        ));
-        }
+                                Component.text("You just joined ")
+                                        .append(
+                                                Component.text(title())
+                                                        .style(Style.style(TextDecoration.UNDERLINED))
+                                        )
+                        )
+        );
+        instance.getPlayers().forEach(p -> {
+            p.sendMessage(
+                    Component.text(player.getUsername()).color(NamedTextColor.AQUA)
+                            .append(Component.text(" joined the meeting!").color(NamedTextColor.WHITE))
+            );
+        });
+        player.sendMessage(Component.text("Here are " + instance.getPlayers().size() + " players in the meeting, including ")
+                .append(
+                        Component.text(instance.getPlayers().stream()
+                                .map(it -> it.getUsername())
+                                .collect(Collectors.joining(", "))).color(NamedTextColor.AQUA)
+                ));
+    }
 }
